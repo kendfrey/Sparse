4ae0b60d593af2a818b7df4395841d5d5f2744de
Kendall Frey <kendfrey@gmail.com>
2013-06-29 11:30:01 -0400

    SyntaxTree class

---
+++ Sparse/SyntaxTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparse
+{
+	public class SyntaxTree
+	{
+		public string RuleName
+		{
+			get;
+			private set;
+		}
+
+		public ReadOnlyCollection<SyntaxTree> SyntaxTrees
+		{
+			get;
+			private set;
+		}
+
+		internal SyntaxTree(string ruleName, SyntaxTree[] syntaxTrees)
+		{
+			this.RuleName = ruleName;
+			this.SyntaxTrees = Array.AsReadOnly(syntaxTrees);
+		}
+	}
+}
